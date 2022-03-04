62fe32d44c2e7453c0915400675f6d857a3852bc
Nando Thomassen <nandothomassen@gmail.com>
2022-03-04 23:13:21 +0100

    Add unit tests
    Add unit test for company name and slogan

--- src/test/kotlin/TestClient.kt
+++ src/test/kotlin/TestClient.kt
@@ -1,12 +1,22 @@
 import kotlinx.browser.document
 import kotlin.test.Test
+import kotlin.test.assertContains
 import kotlin.test.assertEquals
 
 class TestClient {
     @Test
-    fun testSayHello() {
+    fun testCompanyName() {
         val container = document.createElement("div")
         container.home()
-        assertEquals("Hello from JS", container.textContent)
+
+        assertContains(container.textContent!!, "Thynq", true)
+    }
+
+    @Test
+    fun testCompanySlogan() {
+        val container = document.createElement("div")
+        container.home()
+
+        assertContains(container.textContent!!, "Software and technology", true)
     }
 } 
